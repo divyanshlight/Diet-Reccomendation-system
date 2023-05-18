62018b3a37d382db849b4cbde5810c5bd6934323
divyanshlight <111475395+divyanshlight@users.noreply.github.com>
2023-05-18 11:51:30 +0530

    first commit

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Food Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+
+
